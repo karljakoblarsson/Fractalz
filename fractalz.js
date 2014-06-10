8771a17a7c52275355134a5d66da824dd5ccb0fe
Karl Jakob Larsson <k.jakob.larsson@gmail.com>
2014-06-10 13:23:14 +0200

    Moved lots of math functions to a separate file. Still reasearching why addToHist is so slow.

--- fractalz.js
+++ fractalz.js
@@ -9,73 +9,25 @@
  *   - Gamma adjustment
  *   - Vibrancy
  * Final and Post transforms
+ * Combinations of NLFns.
  * Multiple weighted flames
  * Multistage computation and display.
+ * More variations
  * Flame loading
  */
 
+/* ----- 2D Setup ----- */
 var canvas = document.getElementById("a");
 canvas.width *= window.devicePixelRatio; // high-dpi adjustment
 canvas.height *= window.devicePixelRatio;
 var ctx = canvas.getContext("2d");
 ctx.imageSmoothingEnabled = false; // The algorithm takes care of it.
-
-
-// Convinience
-var rand = function(min, max) {
-    return Math.random() * (max - min) + min;
-};
-var round = Math.round;
-var floor = Math.floor;
-var pow = Math.pow;
 var w = canvas.width;
 var h = canvas.height;
-function x(p) { return p[0]; };
-function y(p) { return p[1]; };
 
-function sum(vec) {
-    return vec.reduce(function(a,b) {return a+b;});
-}
-function norm(vec) {
-    return Math.sqrt(sum(vec));
-}
-
-function normalize(vec) {
-    return vec.map(function(elem) {return elem / sum(vec); });
-}
-
-function makeColor(data, index, rgba) {
-    data[index] = rgba[0];
-    data[index+1] = rgba[1];
-    data[index+2] = rgba[2];
-    data[index+3] = rgba[4] || 1;
-}
-
-function addToHist(data, index, rgba) {
-    index *= 4;
-    data[index] += rgba[0];
-    data[index+1] += rgba[1];
-    data[index+2] += rgba[2];
-    data[index+3] += rgba[4] || 1;
-}
-
-function affine(p, a, b, c, d, e, f) {
-    var x = p[0];
-    var y = p[1];
-    return [a*x + b*y + c, d*x + e*y + f];
-}
-
-
-// Define the coordinate-space of the display.
-// The current fractal live in 0<x|y<1
-var space = {x_min: -1,
-             y_min: -1,
-             x_max: 1,
-             y_max: 1
-            };
 
 // Transform from coordinate-space to screen-space.
-function coToPix(p) {
+function coToPix(p, space) {
     var x_len = (space.x_max - space.x_min);
     var y_len = (space.x_max - space.x_min);
 
@@ -87,40 +39,69 @@ function pixToIndex(p) {
     return (p[1] * w + p[0]);
 }
 
+/***** *****/
+function makeColor(data, index, rgba) {
+    data[index] = rgba[0];
+    data[index+1] = rgba[1];
+    data[index+2] = rgba[2];
+    data[index+3] = rgba[4] || 1;
+}
+
+// Using the histogram as a parameter makes the fn 3x slower???
+// The most expensive function in the code. Why?
+function addToHist(index, rgba) {
+    index *= 4;
+    hist[index] += rgba[0];
+    hist[index+1] += rgba[1];
+    hist[index+2] += rgba[2];
+    hist[index+3] += rgba[4] || 1;
+}
+
+
+// Define the coordinate-space of the display.
+var space = {x_min: -1, y_min: -1,
+             x_max: 1,  y_max: 1
+            };
+            
+// I desperatly need a new datastruct for the xforms.
+var fns = [
+    function(x) { return variations[0]( affine(x, 1/2, 0, 0, 0, 1/2, 0) );},
+    function(x) { return variations[1]( affine(x, 1/2, 0, 2, 0, 1/2, 0) );},
+    function(x) { return variations[2]( affine(x, 1/2, 1/5, 0, Math.sqrt(2), 1/2, 2) );},
+    function(x) { return variations[1]( affine(x, 1/3, 0, 3, 0, 1/3, 3) );},
+    function(x) { return variations[2]( affine(x, 1, 0, 0, 0, 1, 0) );},
+];
+var fnColors = [1,1,0.8,0.3,0.1,0.1];
+// functions should also have a weight.
+
 var palette = palette1;
 
-/*function palette(v) {
-    if (v < 2) {return [0,0,0];}
-    else if (v < 100) {return [v/2,v*4,v*2];}
-    else if (v < 190) {return [v*8,v,255-v];}
-    else { return [255, 255, v];}
-}*/
 
-Int32Array.prototype.reduce = function(fn) {
-    var ans = this[0];
-    for (var i = 0; i < this.length; i++) {
-        ans = fn(this[i], ans);
+var img = ctx.createImageData(w,h);
+var hist = new Int32Array(w*h*4);
+var data = img.data;
+
+
+function logDisplay() {
+    img = ctx.createImageData(w,h);
+    for (var i = 0; i < hist.length; i += 4) {
+        var alpha = hist[i+3] || 1 ;
+        var factor = Math.log(alpha) / alpha;
+
+        img.data[i] = hist[i] * factor;
+        img.data[i+1] = hist[i+1] * factor;
+        img.data[i+2] = hist[i+2] * factor;
+        img.data[i+3] = 255;
+
     }
-    return ans;
-};
+}
+
+ctx.fillStyle = "#FFF";
 
 function drawIFS(ctx){
-    // I desperatly need a new datastruct for the xforms.
-    // but first, colors!
-    var fns = [
-        function(x) { return variations[0]( affine(x, 1/2, 0, 0, 0, 1/2, 0) );},
-        function(x) { return variations[1]( affine(x, 1/2, 0, 2, 0, 1/2, 0) );},
-        function(x) { return variations[2]( affine(x, 1/2, 1/5, 0, Math.sqrt(2), 1/2, 2) );},
-        function(x) { return variations[1]( affine(x, 1/3, 0, 3, 0, 1/3, 3) );},
-        function(x) { return variations[2]( affine(x, 1, 0, 0, 0, 1, 0) );},
-    ];
-    var fnColors = [1,1,0.8,0.3,0.1,0.1];
-    // functions should also have a weight.
-
-    var img = ctx.getImageData(0,0,w,h);
-    var hist = new Int32Array(w*h*4);
-    var data = img.data;
-
+    console.log("hej");
+    ctx.clearRect(0,0,w,h);
+    hist = new Int32Array(w*h*4);
     var p = [rand(space.x_min, space.x_max), rand(space.y_min, space.y_max)];
     var c = rand(0,1);
 
@@ -135,31 +116,34 @@ function drawIFS(ctx){
 
         // 20 because the paper says so.
         if (n>20) {
-            var index = pixToIndex(coToPix(p));
+            var index = pixToIndex(coToPix(p, space));
             var color = palette(c);
-            addToHist(hist, index, color);
+            addToHist(index, color);
+        }
+        
+        /*if (n%1e5 === 0) {
+            ctx.clearRect(0,0,w,h);
+            logDisplay();
+            ctx.putImageData(img,0,0);
+            console.log("Paint - partial");
+        }*/
+        
+        if (n%1e5 === 0) {
+            console.log(n/1e5);
         }
     }
     
     var gamma = 3;
-    
-    for (var i = 0; i < hist.length; i += 4) {
-        var alpha = hist[i+3] || 1 ;
-        var factor = Math.log(alpha) / alpha;
 
-        data[i] = hist[i] * factor;
-        data[i+1] = hist[i+1] * factor;
-        data[i+2] = hist[i+2] * factor;
-        data[i+3] = 255;
-
-    }
+    logDisplay();
 
     // The image will be plotted point-by-point in the loop for complex flames.
+    //ctx.clearRect(0,0,w,h);
     ctx.putImageData(img,0,0);
+    
 }
 
-/* ------------------------------------------------------------------------- */
-// Variations
+/* ------------------- Variations ------------------------ */
 
 function r2(p) { return  pow(x(p), 2) + pow(y(p), 2); }
 function r(p) { return Math.sqrt(r2(p)); };
@@ -180,7 +164,8 @@ var variations = {
  * A function has linear combination of one or more variations.
  */
 
-// Plot it!
+/* ---- Plot it! ----- */
 // The image should be plotted point-by-point in the loop for complex flames.
 // But it make dev faster to have it continuously re-render.
-window.setInterval(drawIFS, 1000, ctx);
+//window.setInterval(drawIFS, 5000, ctx);
+drawIFS(ctx);
---
+++ kjl-math.js
@@ -0,0 +1,43 @@
+/* kjl-Math
+ * assorted missing math functions for js.
+ * graphics related mostly.
+ * mostly with bad implementations. :P
+ 
+ * Karl Jakob Larsson 2014
+ * k.jakob.larsson@gmail.com
+ * MIT License
+ */
+ 
+var rand = function(min, max) {
+    return Math.random() * (max - min) + min;
+};
+var round = Math.round;
+var floor = Math.floor;
+var pow = Math.pow;
+function x(p) { return p[0]; };
+function y(p) { return p[1]; };
+
+function sum(vec) {
+    return vec.reduce(function(a,b) {return a+b;});
+}
+function norm(vec) {
+    return Math.sqrt(sum(vec));
+}
+
+function normalize(vec) {
+    return vec.map(function(elem) {return elem / sum(vec); });
+}
+
+function affine(p, a, b, c, d, e, f) {
+    var x = p[0];
+    var y = p[1];
+    return [a*x + b*y + c, d*x + e*y + f];
+}
+
+Int32Array.prototype.reduce = function(fn) {
+    var ans = this[0];
+    for (var i = 0; i < this.length; i++) {
+        ans = fn(this[i], ans);
+    }
+    return ans;
+};
